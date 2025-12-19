aa96870c4d66a34180bf06ebf113d57e262e0126
meg44k <yugomegane@gmail.com>
2025-12-19 12:55:46 +0900

    Add: outputs.tfを追加しました

---
+++ terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "frontend_url" {
+  value = azurerm_container_app.frontend.ingress[0].fqdn
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
